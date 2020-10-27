ad141bf3faf25dc82307a66efa49f459766a8eef
Martin Slaby <martinslaby70@gmail.com>
2020-10-27 10:06:30 +0100

    smooth cube transition

--- src/components/3d/cube.tsx
+++ src/components/3d/cube.tsx
@@ -8,8 +8,10 @@ interface props {
 const Cube = ({action}: props) => {
     
     const mesh = useRef<THREE.Mesh>(null);
+
     const [boxColor, setBoxColor] = useState('wheat');
-    
+    let speed = 0.005;
+
     useEffect(() => {
         if (action === 'stop')
             setBoxColor('#b22222');
@@ -21,13 +23,14 @@ const Cube = ({action}: props) => {
 
         if (boxColor !== '#b22222')
         {
-            if (document.getElementById('themer')!.classList.contains('light') && boxColor !== ('#2a2b31'))
+            const themerClassList = document.getElementById('themer')!.classList;
+            if (themerClassList.contains('light') && boxColor !== ('#2a2b31'))
                 setBoxColor('#2a2b31')
-            if (document.getElementById('themer')!.classList.contains('dark') && boxColor !== '#f5deb3')
+            if (themerClassList.contains('dark') && boxColor !== '#f5deb3')
                 setBoxColor('#f5deb3')
         }
 
-       if (mesh.current)
+       if (mesh.current) 
        {
         
             if (action === 'reverse')
@@ -35,7 +38,13 @@ const Cube = ({action}: props) => {
             else if (action === 'slow')
                 mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z -= 0.002; 
             else if (action === 'stop')
-                mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z;
+            {
+                if (speed > 0)
+                {
+                    speed = speed - 0.00005;
+                    mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z -= speed;
+                }              
+            }     
             else
                 mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z -= 0.005; 
 
