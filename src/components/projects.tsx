fcf6dbdb31ae8fc08e8c4d0693898cf635eee89a
Martin Slabý <martin.slaby29@seznam.cz>
2020-11-25 16:37:47 +0100

    projects update

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -14,17 +14,20 @@ const Projects = ({setAction}: props) => {
         <div className="section" id="projects">
             <h2>My projects</h2>
             <div className="projects-container">
-                <div className="project">
+            <div className="project">
                     <div className="summary">
                         <h3>Movie App</h3>
                         {/* <p>Search for your favorite movies/series/episodes in the search bar, except for viewing basic information, you can also interact with your movie list. if the whisperer wont help you find your movie, you can press the small search icon and cycle in multiple search results</p> */}
-                        <ul>
-                            <li>React - Typescript</li>
-                            <li>React Hooks</li>
-                            <li>Axios</li>
-                            <li>SCSS</li>
-                            <li>React-Bootstrap</li>
-                        </ul>
+                        <div className="lists">
+                            <ul>
+                                <li><h4>Client</h4></li>
+                                <li>React - Typescript</li>
+                                <li>React Hooks</li>
+                                <li>Axios</li>
+                                <li>SCSS</li>
+                                <li>React-Bootstrap</li>
+                            </ul>
+                        </div>
                         <a href="https://movies-drab.vercel.app" target="_blank">live demo</a>
                         <a href="https://github.com/martinslaby70/movies" target="_blank">code</a>
                     </div>
@@ -32,18 +35,44 @@ const Projects = ({setAction}: props) => {
                         <img src={image} alt=""/>
                     </div>
                 </div>
-               <div className="project">
-                    <div className="unavalible" onMouseEnter={() => setAction('stop')} onMouseLeave={() => setAction('')}>
-                        <a href="https://github.com/martinslaby70/graphql" target="_blank">
-                            <h3>GraphQL &amp; Webscraping project</h3>
-                            <p className="silent">work in progress...</p>
-                        </a>
+                <div className="project">
+                    <div className="summary">
+                        <h3>Book List</h3>
+                        <div className="lists">
+                            <ul>
+                                <li><h4>Client</h4></li>
+                                <li>React - Typescript</li>
+                                <li>React Hooks</li>
+                                <li>GraphQL - Apollo</li>
+                                <li>SCSS</li>
+                            </ul>
+                            <ul>
+                                <li><h4>Server</h4></li>
+                                <li>Node.js</li>
+                                <li>express.js</li>
+                                <li>GraphQL</li>
+                                <li>MongoDB</li>
+                                <li>mongoose.js</li>
+                            </ul>
+                        </div>      
+                        <a href="/" target="_blank" className="unavalible" onMouseEnter={() => setAction('stop')} onMouseLeave={() => setAction('')}>live demo</a>                  
+                        <a href="https://github.com/martinslaby70/BookList" target="_blank">code</a>
                     </div>
+                    <div className="image-container">
+                        <img src={image} alt=""/>
+                    </div>
+                </div>
+               <div className="project">
                     <div className="unavalible notAllowed" onMouseEnter={() => setAction('stop')} onMouseLeave={() => setAction('')}>
                         <h3>Unit Testing project (Jest)</h3>
                         <p className="silent">upcoming</p>
                     </div>
+                    <div className="unavalible notAllowed" onMouseEnter={() => setAction('stop')} onMouseLeave={() => setAction('')}>
+                        <h3>Next.js 9roject</h3>
+                        <p className="silent">upcoming</p>
+                    </div>
                </div>
+               
                 
             </div>
         </div>
